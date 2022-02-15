d745103cee7844347ddb0c546b7f38d9b88e0d59
amoel <amoelgeogy@gmail.com>
2022-02-15 14:47:54 +0200

    working video from html to django

---
+++ apptest/videotest2/videoproj2/README.md
@@ -0,0 +1,6 @@
+### Progress
+
+Uses django and the pytorch model (no javascript model) <br>
+
+![1](videoproj2/screenshots/1.png)<br><br>
+![1](videoproj2/screenshots/2.gif)<br><br>
---
+++ apptest/videotest2/videoproj2/videoapp2/vgg.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset
+import os
+
+class Vgg(nn.Module):
+    def __init__(self, drop=0.2):
+        super().__init__()
+
+        self.conv1a = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
+        self.conv1b = nn.Conv2d(64, out_channels=64, kernel_size=3, padding=1)
+
+        self.conv2a = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2b = nn.Conv2d(128, 128, 3, padding=1)
+
+        self.conv3a = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3b = nn.Conv2d(256, 256, 3, padding=1)
+
+        self.conv4a = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4b = nn.Conv2d(512, 512, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.bn1a = nn.BatchNorm2d(64)
+        self.bn1b = nn.BatchNorm2d(64)
+
+        self.bn2a = nn.BatchNorm2d(128)
+        self.bn2b = nn.BatchNorm2d(128)
+
+        self.bn3a = nn.BatchNorm2d(256)
+        self.bn3b = nn.BatchNorm2d(256)
+
+        self.bn4a = nn.BatchNorm2d(512)
+        self.bn4b = nn.BatchNorm2d(512)
+
+        # self.lin1 = nn.Linear(512 * 2 * 2, 4096)
+        self.lin1 = nn.Linear(512 * 3 * 3, 4096)
+        self.lin2 = nn.Linear(4096, 4096)
+
+        # self.lin3 = nn.Linear(4096, 7)
+        # output size changed to 9 because we're using the fer plus labels, not the fer 2013 labels
+        self.lin3 = nn.Linear(4096, 9)
+
+        self.drop = nn.Dropout(p=drop)
+
+    def forward(self, x):
+        x = F.relu(self.bn1a(self.conv1a(x)))
+        x = F.relu(self.bn1b(self.conv1b(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn2a(self.conv2a(x)))
+        x = F.relu(self.bn2b(self.conv2b(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn3a(self.conv3a(x)))
+        x = F.relu(self.bn3b(self.conv3b(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn4a(self.conv4a(x)))
+        x = F.relu(self.bn4b(self.conv4b(x)))
+        x = self.pool(x)
+        
+        # x = x.view(-1, 512 * 2 * 2)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.drop(self.lin1(x)))
+        x = F.relu(self.drop(self.lin2(x)))
+        x = self.lin3(x)
+        
+        return x
+
+vggmodel = Vgg()
+
+device = torch.device('cpu')
+
+vggmodel.load_state_dict(torch.load('saved_model/vggmodel_state_dict.pth', map_location=device))
